35b91b1fdf15173be743d4d726a5fe75b83bc321
emir-munoz <emir@emunoz.org>
2014-08-12 20:35:42 +0100

    First commit

---
+++ src/main/java/org/ki2na/ld4ie/App.java
@@ -0,0 +1,13 @@
+package org.ki2na.ld4ie;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
